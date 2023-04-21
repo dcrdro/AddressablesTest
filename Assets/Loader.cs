3fb27cd5a8a0a2712bd7c095eeb0d232441d90f9
Ruslan.S <ruslan.smirnov@rqgames.net>
2023-04-21 15:36:42 +0300

    add path & cylinder

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -1,38 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.Networking;
 using UnityEngine.ResourceManagement.ResourceLocations;
 
 public class Loader : MonoBehaviour
 {
-    public AssetReference reff;
-
     public string key;
-    public string key2;
-    public string path;
+
+    public string url =
+        "https://dcrdro.github.io/AddressablesTest/ServerData/StandaloneOSX/catalog_2023.04.21.09.33.27.hash";
     
     // Start is called before the first frame update
     async void Start()
     {
+        StartCoroutine(TestDownload());
+        
         var locs = await Addressables.InitializeAsync().Task;
 
-        var l = await Addressables.GetDownloadSizeAsync(key).Task;
-        Debug.Log("l = " + l);
+        
+        // Addressables.ClearDependencyCacheAsync(locs);
+        // await Task.Delay(2000);
+        
+        foreach (var k in locs.Keys)
+        {
+            Debug.Log("k = " + k);
+        }
+        
+        // var l = await Addressables.GetDownloadSizeAsync(locs.Keys).Task;
+        // Debug.Log("l = " + l);
+        
+        await Addressables.DownloadDependenciesAsync(key).Task;
+
+        // await Addressables.InstantiateAsync(key).Task;
+        
         //
         // var l2 = await Addressables.GetDownloadSizeAsync(key2).Task;
         // Debug.Log("l2 = " + l2);
 
         // reff.InstantiateAsync(Vector3.zero, Quaternion.identity);
-        
+
         // Addressables.LoadResourceLocationsAsync()
         // foreach (var k in locs.Keys)
         // {
         //     Debug.Log("k = " + k);
         // }
-        
+
         // await Addressables.InstantiateAsync("cube").Task;
+    }
+
+    IEnumerator TestDownload()
+    {
+        using UnityWebRequest r = UnityWebRequest.Get(url);
+        var rr = r.SendWebRequest();
+        yield return rr;
+
+        Debug.Log("download = " + r.downloadHandler.text);
     }
 
     // Update is called once per frame
